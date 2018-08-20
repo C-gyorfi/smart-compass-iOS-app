a8a3c1b595781cfab004968cdf3398b7142730fa
Csabi <csaba2.gyorfi@live.uwe.ac.uk>
2018-08-20 15:01:58 +0100

    Initial Commit

---
+++ Compass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Compass
+//
+//  Created by Csabi on 20/08/2018.
+//  Copyright © 2018 Csabi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
